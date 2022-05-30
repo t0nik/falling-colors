a88936319e657c08bc016fe5bf537a68a52edb7d
t0nik <59863144+t0nik@users.noreply.github.com>
2022-05-30 03:52:38 +0200

    TileMap and ColorPicker Components

--- src/index.tsx
+++ src/index.tsx
@@ -3,37 +3,90 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 
-class Tile extends React.Component {
-  redner() {
-    return (<div className="tile">a</div>);
-  }
+type Props = {
+  onMouseOver : VoidFunction;
 }
 
-class TileMap extends React.Component {
+type States = {
+  color : string;
+}
+
+class Tile extends React.Component<Props, States> {
+  constructor(props : any) {
+    super(props);
+
+    this.state = {
+      color: "#C5C5C5",
+    }
+  }
 
   render() {
     return (
-    <div>
-      <Tile />
-      <Tile />
-    </div>);
+      <div className="tile"
+        onMouseOver={() => this.setState({ color: "#FF0000" })}
+        onMouseOut={() => this.setState({ color: "#C5C5C5" })}
+        style={{ backgroundColor: this.state.color }}
+      ></div>
+    );
   }
-}
+};
+
+class TileMap extends React.Component {
+
+  // TODO: Array of Tiles as a state, values of array are color: string
+
+  renderTile(i : number) {
+    return (
+      <Tile onMouseOver={() => this.handleMouseOver(i)}/>
+      );
+  }
+
+  handleMouseOver(i : number) {
+    // TODO
+  }
+
+  // Add handleMouseOut
+
+  render() {
+    const rows = 20; 
+    const cols = 20;
+
+    return (
+      <div>
+        {[...new Array(rows)].map((x, rowIndex) => {
+            return (
+              <div className="tile-row" key={rowIndex}>
+                {[...new Array(cols)].map((y, colIndex) => this.renderTile(rowIndex*cols + colIndex))}
+              </div>
+            );
+          }
+        )
+        }
+      </div>
+    );
+  }
+};
 
 class ColorPicker extends React.Component {
 
-}
+};
 
 class App extends React.Component {
-  render(){
+  render() {
     return (
       <div className="app">
-        <div className="app-tilemap">Map: {/*TileMap*/}</div>
-        <div className="app-colorpicker">Color picker: {/*Color picker*/}</div>
+        <div className="app-tilemap">
+          <div className="desc">Map (put your cursor over the tiles): </div>
+          <TileMap />
+        </div>
+        <div className="app-colorpicker">
+          <div className="desc">Color picker: TODO </div>
+          Current color: #FF00000 {}
+        </div>
       </div>
     )
   };
-}
+};
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
