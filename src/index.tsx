3fdc158ae24b3083135304a6af7a2c83b1441f69
t0nik <59863144+t0nik@users.noreply.github.com>
2022-06-01 01:10:06 +0200

    TileMap improvements

--- src/index.tsx
+++ src/index.tsx
@@ -1,62 +1,88 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { isPropertySignature } from 'typescript';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 
+const g_rows = 20; 
+const g_cols = 20;
+const g_falling_time = 100; // One tile falling time in milliseconds
+
 type Props = {
   onMouseOver : VoidFunction;
-}
-
-type States = {
+  onMouseOut : VoidFunction;
   color : string;
 }
 
-class Tile extends React.Component<Props, States> {
-  constructor(props : any) {
-    super(props);
+type TileMapStates = {
+  tiles: Array<string>;
+}
 
-    this.state = {
-      color: "#C5C5C5",
-    }
-  }
-
+class Tile extends React.Component<Props> {
   render() {
     return (
       <div className="tile"
-        onMouseOver={() => this.setState({ color: "#FF0000" })}
-        onMouseOut={() => this.setState({ color: "#C5C5C5" })}
-        style={{ backgroundColor: this.state.color }}
+        onMouseOver={() => this.props.onMouseOver()}
+        onMouseOut={() => this.props.onMouseOut()}
+        style={{ backgroundColor: this.props.color }}
       ></div>
     );
   }
 };
 
-class TileMap extends React.Component {
+class TileMap extends React.Component<{},TileMapStates> {
 
-  // TODO: Array of Tiles as a state, values of array are color: string
+  constructor(props : any) {
+    super(props);
+
+    this.state = {
+      tiles: Array(g_rows*g_cols).fill("#C5C5C5"),
+    }
+  }
 
   renderTile(i : number) {
     return (
-      <Tile onMouseOver={() => this.handleMouseOver(i)}/>
+      <Tile onMouseOver={() => this.handleMouseOver(i)}
+      onMouseOut={() => this.handleMouseOut(i)}
+      color={this.state.tiles[i]}
+      />
       );
   }
 
   handleMouseOver(i : number) {
-    // TODO
+    let temp_tiles = this.state.tiles;
+
+        temp_tiles[i] = "#FF0000";
+        this.setState({tiles: temp_tiles});
+
+      // Draw single, falling tile
+      for (let it = g_rows; it < g_rows*g_cols; it += g_rows) {
+        setTimeout(() => {
+          temp_tiles[i+it-g_rows] = "#C5C5C5"; // Modify previous tile to tilemap backround color.
+          temp_tiles[i+it] = "#FF0000";
+          this.setState({tiles: temp_tiles});
+        }, g_falling_time * (it - 1)/g_rows); // each callback occurs `g_falling_time` ms after the last one
+      }
+    
+    return;
   }
 
-  // Add handleMouseOut
+  
+  handleMouseOut(i : number) {
+    // let temp_tiles = this.state.tiles;
+    // for (let it = 0; it < g_rows*g_cols; it += g_rows) {
+    //   temp_tiles[i+it] = "#C5C5C5";
+    // }
+    // this.setState({tiles: temp_tiles});
+  }
 
   render() {
-    const rows = 20; 
-    const cols = 20;
-
     return (
       <div>
-        {[...new Array(rows)].map((x, rowIndex) => {
+        {[...new Array(g_rows)].map((x, rowIndex) => {
             return (
               <div className="tile-row" key={rowIndex}>
-                {[...new Array(cols)].map((y, colIndex) => this.renderTile(rowIndex*cols + colIndex))}
+                {[...new Array(g_cols)].map((y, colIndex) => this.renderTile(rowIndex*g_cols + colIndex))}
               </div>
             );
           }
@@ -81,7 +107,7 @@ class App extends React.Component {
         </div>
         <div className="app-colorpicker">
           <div className="desc">Color picker: TODO </div>
-          Current color: #FF00000 {}
+          Current color: <div className="picked-color">#FF00000</div> {}
         </div>
       </div>
     )
